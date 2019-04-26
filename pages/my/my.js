a8288b745e3e94a6a433ed083795d9e822f43a69
unknown <unknown@example.com>
2019-04-26 20:08:26 +0800

    one

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,14 +1,4 @@
 // pages/my/my.js
 Page({
-  data: {
-    gridList: [
-      { enName: 'favorite', zhName: '历年真题' },
-      { enName: 'history', zhName: '真题答案' },
-      { enName: 'shake', zhName: '听力' },
-      { enName: 'gallery', zhName: '阅读' },
-      { enName: 'setting', zhName: '翻译' },
-      { enName: 'gallery', zhName: '写作' }
-
-    ],
-  },
+ 
 })
